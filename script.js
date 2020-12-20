9ff6449d0167c804589ddaac65b7e3cc54dd8095
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-12-19 20:52:46 -0500

    Added functionality so that the modal can now open and close when the Sign Up button and exit (X) button are clicked on, respectively. Added animation for the duration of the modal when it fades in and out.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const toggle = document.getElementById("toggle");
-const close = document.getElementById("close");
 const open = document.getElementById("open");
+const close = document.getElementById("close");
 const modal = document.getElementById("modal");
 
-// Toggle Navigation
+// Toggle navigation (click event for toggle icon).
 toggle.addEventListener("click", () =>
   document.body.classList.toggle("show-nav")
 );
+
+// Show modal (click event for Sign Up button).
+open.addEventListener("click", () => modal.classList.add("show-modal"));
+
+// Hide modal (click event for X button).
+close.addEventListener("click", () => modal.classList.remove("show-modal"));
