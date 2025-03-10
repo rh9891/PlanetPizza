cf15d6344f04cfb532c4a0089a82adfeb477b8a9
rh9891 <romie.hecdivert@gmail.com>
2025-03-09 23:55:37 -0400

    Completed header styling and functionality.

---
+++ main.js
@@ -0,0 +1,20 @@
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+const navLink = document.querySelectorAll('.nav__link');
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+navLink.forEach(link => link.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+}
